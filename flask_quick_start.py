bbfb1943e16e846f6ebff6d71feecd2d490e4e18
Thomas <mcdonnellthomas89@gmail.com>
2018-08-24 18:03:35 +0100

    virtualenv creation and requirements installation.

--- flask_quick_start.py
+++ flask_quick_start.py
@@ -1,4 +1,10 @@
-import argparse, os, sys, shutil
+# -*- coding: utf-8 -*-
+
+import argparse
+import shutil
+import os
+import sys
+import subprocess
 
 """
 Parser Args
@@ -11,6 +17,7 @@ parser.add_argument('appname', metavar='A', type=str, nargs=1, help='The app\
 parser.add_argument('-p', '--path', type=str, nargs=1, default=[os.getcwd()],
                     help='The path of new project.')
 parser.add_argument('-s', '--structure', type=str, nargs=1, help='Structure to use.')
+parser.add_argument('-v', '--virtualenv', action='store_true')
 args = parser.parse_args()
 
 """
@@ -25,8 +32,31 @@ def create_project(appname, project_path, structure):
     shutil.copytree(struct, project)
 
 # vertualenv creation as venv into project directory 
-def verlualenv():
-    pass
+def init_virtualenv(project_path, appname):
+    project = os.path.join(project_path, appname)
+    output, error = subprocess.Popen(
+        [
+            'virtualenv',
+            os.path.join(project, 'venv')
+        ],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE).communicate()
+    if error:
+        print(f'An error occured:\n {error}')
+        sys.exit(2)
+    venv_bin = os.path.join(project, 'venv/bin')
+    output, error = subprocess.Popen(
+        [
+            os.path.join(venv_bin, 'pip'),
+            'install',
+            '-r',
+            os.path.join(project, 'requirements.txt')
+        ],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE).communicate()
+    if error:
+        print(f'An error occured during requiremets installation: \n{error}')
+        sys.exit(2)
 
 # git init
 def init_git():
@@ -34,3 +64,4 @@ def init_git():
 
 if __name__ == "__main__":
     create_project(args.appname[0], args.path[0], args.structure[0])
+    init_virtualenv(args.path[0], args.appname[0])
